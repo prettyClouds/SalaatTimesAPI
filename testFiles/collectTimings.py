f5817073eb3306375d2304c567cb9f7d43bf11f4
Murtuza Boxwala <1mboxwala2@gmail.com>
2014-02-08 20:27:42 -0500

    updating java sunrise lib
    1) Added the joda datetime library to make shifting between timezones
    easier
    2) reorganized the code a little (still lots of work to do here)
    3) made the script that hits mumineen.org a little friendlier

--- testFiles/collectTimings.py
+++ testFiles/collectTimings.py
@@ -1,17 +1,27 @@
 import requests
 import json
+import sys
 
+season = sys.argv[1]
+if season == 'w':
+    date = "2000-12-21"
+
+elif season == 's':
+    date = "2000-06-21"
+
+lat = -65.0
+lng = 0.0
+if len(sys.argv) == 3:
+    lng = float(sys.argv[2])
 
 API = "http://api.mumineen.org/salaat"
 #?lat=%f&lng=%f&timezone=%s&start=%s&end=%s" 
-lat = -65.0
-lng = 0.0
-date = "2000-12-21"
-f = open('%s_MDO_timings.txt' %(date), 'w')
+
+f = open('MDO_timings_%s_lon%.3f.txt' %(date, lng), 'w')
 
 #f.write("%s,%s,%s\n" %("fajr","sunrise","maghrib"))
 for x in range(-1,26):
-    f.write("%f, %f, %s\n" %(lat,lng,date))
+    f.write("%.3f, %.3f, %s\n" %(lat,lng,date))
     #formatString %(lat + 5*x, lng, timeZone, date, date)
     r = requests.get(API,params={"lat": lat, "lng": lng, "timezone":"Etc/UTC", "start":date, "end":date })
     #print r.json()
